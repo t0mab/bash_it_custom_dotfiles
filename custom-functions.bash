d501c87ea49b6615474fde168c2e6840b1ca40e8
t0mab <baguet@unistra.fr>
2013-03-21 20:49:25 +0100

    Fix bad typo on function's name

--- custom-functions.bash
+++ custom-functions.bash
@@ -14,7 +14,7 @@
 # MISC
 #
 
-function calibre_uprgrade()
+function calibre_upgrade()
 {
     sudo python -c "import sys; py3 = sys.version_info[0] > 2; u = __import__('urllib.request' if py3 else 'urllib', fromlist=1); exec(u.urlopen('http://status.calibre-ebook.com/linux_installer').read()); main(install_dir='~/bin')"
 }
