469c18fe45faf83a37556da400e5b7bd041060b6
Thomas Baguet <baguet@unistra.fr>
2013-07-26 11:59:26 +0200

    add images and font base64 encoding for css toying

--- custom-functions.bash
+++ custom-functions.bash
@@ -232,3 +232,37 @@ rdns() {
     dig +short $1 | xargs -L 5 nslookup | grep name
 }
 alias rnds='rdns'
+
+pdf2booklet(){
+if [[ $# != 2 ]]; then
+   echo "usage pdf2booklet <input> <output>" >&2
+   else
+   pdf2ps $1 - | psbook | psnup -2 | ps2pdf - $2
+fi
+}
+
+# encode a given image file as base64 and output css background property to clipboard
+function 64enc() {
+
+openssl base64 -in $1 | awk -v ext="${1#*.}" '{ str1=str1 $0 }END{ print "background:url(data:image/"ext";base64,"str1");" }'|pbcopy
+
+echo "$1 encoded to clipboard"
+}
+
+
+# Quickly get image dimensions from the command line
+function imgsize() {
+	local width height
+	if [[ -f $1 ]]; then
+		height=$(sips -g pixelHeight "$1"|tail -n 1|awk '{print $2}')
+		width=$(sips -g pixelWidth "$1"|tail -n 1|awk '{print $2}')
+		echo "${width} x ${height}"
+	else
+		echo "File not found"
+	fi
+}
+
+function 64font() {
+	openssl base64 -in $1 | awk -v ext="${1#*.}" '{ str1=str1 $0 }END{ print "src:url(\"data:font/"ext";base64,"str1"\")  format(\"woff\");" }'|pbcopy
+	echo "$1 encoded as font and copied to clipboard"
+}
